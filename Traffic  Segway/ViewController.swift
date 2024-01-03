acc274f3564440d857905f7d22cd03a9964c56d1
Lenayah <lfanesie@gmail.com>
2024-01-03 14:16:57 -0500

    update

--- Traffic  Segway/ViewController.swift
+++ Traffic  Segway/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var isOn: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) { }
 
-
+  
+    @IBAction func gotoyellow(_ sender: Any) {
+        
+        if isOn.isOn == true{
+            performSegue(withIdentifier: "purple", sender: nil)
+        }
+    }
+    @IBAction func gotopurple(_ sender: Any) {
+        
+        if isOn.isOn == true{
+            performSegue(withIdentifier: "yellow", sender: nil)
+        }
+    }
 }
 
